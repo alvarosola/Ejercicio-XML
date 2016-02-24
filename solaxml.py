446aa92dd250fb2a730f7f18d25e65b41f17f6e8
alvaro sola <oalvarosola@gmail.com>
2016-02-24 14:09:26 +0100

    He añadido el fichero solaxml, que es mi fichero de proyecto de ejercicios xml.

---
+++ solaxml.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*--
+
+#Ejercicio-XML
+
+#1 - Lista los animales que hay en adopción con su nombre y raza.
+
+#2 - Muestra cuantos animales hay y el total de razas.
+
+#3 - Pide por teclado años o meses, despues vuelve a pedir por teclado una edad mínima y edad máxima.
+
+#4 - Pide por teclado una especie y que muestre todos los animales disponibles de esa especie.
+
+#5 - Pide por teclado el tamaño de los animales y te diga de que especies hay disponibles de ese tamaño.
